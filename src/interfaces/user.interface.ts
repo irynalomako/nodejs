75a1c6a75f1e32b26564771f36b3a63a0b8613cb
irynalomako <lomakoiryna86@gmail.com>
2025-03-31 00:25:18 +0300

    lesson3

---
+++ src/interfaces/user.interface.ts
@@ -0,0 +1,9 @@
+export interface IUser {
+    _id: string,
+    name: string,
+    surname: string,
+    age: number,
+    createdAt: Date,
+    updatedAt: Date
+}
+export type IUserDTO = Pick< IUser, "name" | "surname" | "age" >
---
+++ src/main.ts
@@ -0,0 +1,36 @@
+import express from "express"
+import mongoose from "mongoose";
+import {config} from "./configs/config";
+import {apiRouter} from "./routers/api.router";
+
+const app = express();
+
+app.use (express.json());
+app.use (express.urlencoded({extended: true}));
+app.use ('/', apiRouter);
+
+const bdConnection = async () => {
+    let dbCon = false;
+
+    while (!dbCon) {
+    try {
+        console.log ('Connecting to DB...');
+        await mongoose.connect(config.MONGO_URI);
+        dbCon = true
+    } catch (e) {
+        console.log ('Database unavailable, wait 3 seconds');
+        await new Promise (resolve => setTimeout (resolve, 3000))
+    }
+}};
+
+const start = async () => {
+    try{
+        await bdConnection();
+        app.listen(config.PORT, ()=> {
+            console.log (`Server listening on ${config.PORT}`)
+        })
+    }catch (e) {
+        console.log (e)
+    }
+}
+start ();
